b534a327d8c43c3ce7a36aa5070f80b9b747b3e3
appitAS <faiz@appitsoftware.com>
2025-06-10 11:03:48 +0530

    Update JobCards.jsx

--- src/Components/JobCards.jsx
+++ src/Components/JobCards.jsx
@@ -259,6 +259,7 @@ const JobCard = () => {
               >
                 <Button
                   href={job.job_url}
+                  target="_blank"
                   variant="contained"
                   fullWidth
                   sx={{
